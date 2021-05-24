7a363ee8ba066349aafeda10855e70aa9046b591
gnprofessor <jackdan0308>
2021-05-24 17:07:24 +0300

    Добавил Car

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,18 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+}
